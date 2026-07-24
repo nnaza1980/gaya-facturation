937f9091aca7259cc2b1f668e70e9efdb32201ca
nnaza1980 <n.nazaretian@agence-gaya.com>
2026-07-24 22:52:12 +0200

    Add basePath and assetPrefix to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  basePath,
+  assetPrefix: basePath || undefined,
+  trailingSlash: true,
 };
 module.exports = nextConfig;
